712ac1ef36eb3fd7ec7c6a6ec0adb56df742d0ed
PublicStringLeonBoller <bollerleon89@gmail.com>
2021-10-27 16:02:29 -0300

    deleted files

--- Pods/EyesXCUI/EyesXCUI/EyesXCUI.xcframework/ios-arm64_armv7/EyesXCUI.framework/Headers/RegionProviderProtocol.h
+++
@@ -1,9 +0,0 @@
-// Licensed under the Applitools SDK License, which can be found here: https://www.applitools.com/eula/sdk
-
-#import "Region.h"
-
-@protocol RegionProviderProtocol <NSObject>
-
-- (Region *)getRegion;
-
-@end
